07dc4f3221b6e4f49c761a17ff3fd366f23bb19a
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-04-01 14:46:28 +0200

    Add styling with Tailwind CSS

--- web-interface/tailwind.config.js
+++ web-interface/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
       "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            bgPrimary: '#1e1d1d',
+            bgSecondary: '#000000',
+            primary: '#ffffff',
+            secondary: '#a8a8a8'
+        }
+    },
   },
   plugins: [],
 }
