d40eda9c2f15d87fb5e848f61eec953ac92e8d92
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-12-26 20:03:07 +0100

    Remove builder annotation

--- backend/src/main/java/com/jakubdeniziak/librarian/library/dto/LibraryRequest.java
+++ backend/src/main/java/com/jakubdeniziak/librarian/library/dto/LibraryRequest.java
@@ -1,10 +1,8 @@
 package com.jakubdeniziak.librarian.library.dto;
 
-import lombok.Builder;
 import lombok.Value;
 
 @Value
-@Builder
 public class LibraryRequest {
 
     String name;
--- backend/src/main/java/com/jakubdeniziak/librarian/publisher/dto/PublisherRequest.java
+++ backend/src/main/java/com/jakubdeniziak/librarian/publisher/dto/PublisherRequest.java
@@ -1,10 +1,8 @@
 package com.jakubdeniziak.librarian.publisher.dto;
 
-import lombok.Builder;
 import lombok.Value;
 
 @Value
-@Builder
 public class PublisherRequest {
 
     String name;
