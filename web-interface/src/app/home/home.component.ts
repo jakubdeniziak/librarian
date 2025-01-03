ecf8619415dd0327b0d60428f165fe12bb866294
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-01-03 18:36:25 +0100

    Add button to download all data

--- web-interface/src/app/home/home.component.ts
+++ web-interface/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from "../data/service/data.service";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+    constructor(private dataService: DataService) { }
+
+    downloadFile() {
+        this.dataService.fetchData().subscribe({
+            next: (data) => {
+                const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+                saveAs(blob, 'data.json');
+            }
+        });
+    }
 
 }
