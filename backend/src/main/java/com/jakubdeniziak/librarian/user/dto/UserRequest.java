ef1ed29f4ca7d6e705088a566ab11a2fb7d725b0
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-01-05 16:27:55 +0100

    Remove id from user creation request

--- backend/src/main/java/com/jakubdeniziak/librarian/user/dto/UserRequest.java
+++ backend/src/main/java/com/jakubdeniziak/librarian/user/dto/UserRequest.java
@@ -2,12 +2,9 @@ package com.jakubdeniziak.librarian.user.dto;
 
 import lombok.Value;
 
-import java.util.UUID;
-
 @Value
 public class UserRequest {
 
-    UUID id;
     String firstName;
     String lastName;
     String nickname;
