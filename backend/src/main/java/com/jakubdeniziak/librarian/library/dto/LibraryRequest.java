94e1e6412a032bb420ecfb0968acbcb2b675ebd0
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-03-18 20:10:37 +0100

    Add validation for libraries

--- backend/src/main/java/com/jakubdeniziak/librarian/library/dto/LibraryRequest.java
+++ backend/src/main/java/com/jakubdeniziak/librarian/library/dto/LibraryRequest.java
@@ -1,12 +1,17 @@
 package com.jakubdeniziak.librarian.library.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 @Value
 public class LibraryRequest {
 
+    @NotBlank
     String name;
+    @NotBlank
     String address;
+    @Size(max = 1000)
     String description;
 
 }
