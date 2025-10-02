b2642121fbef17c4e1c642a7eeebe5e5ca6accd4
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-10-02 21:42:51 +0200

    Add author fields in frontend

--- frontend/src/app/features/author/models/author-details.model.ts
+++ frontend/src/app/features/author/models/author-details.model.ts
@@ -3,4 +3,11 @@ export interface AuthorDetails {
   firstName: string;
   lastName: string;
   description: string;
+  pictureUrl: string;
+  aliases: string[];
+  genres: string[];
+  birthDate: Date;
+  deathDate: Date;
+  lastEditedTime: Date;
+  lastEditedBy: string;
 }
